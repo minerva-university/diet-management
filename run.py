e9bd57576fb42121b5ba50876ae30367e732f4c2
potato <93452403+Shadimrad@users.noreply.github.com>
2023-04-16 04:28:00 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+import subprocess
+
+# Define the file names for insert_meals.py and server.py
+insert_meals_script = 'insert_meals.py'
+server_script = 'server.py'
+
+# Run the insert_meals.py script
+subprocess.run(['python', insert_meals_script])
+
+# Run the server.py script
+subprocess.run(['python', server_script])
