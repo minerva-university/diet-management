62274c8db512be4b65b81a58179580e304458f8a
Shaghayegh <93452403+Shadimrad@users.noreply.github.com>
2023-04-19 14:14:18 +0530

    Update meal_planner_test.py

--- unit_test/meal_planner_test.py
+++ unit_test/meal_planner_test.py
@@ -15,17 +15,24 @@ def mock_user_calories():
     return UserCalories(user_id=1, calories=2000)
 
 
+class MockQuery:
+    def __init__(self, items):
+        self.items = items
+
+    def filter(self, label_expression):
+        label = str(label_expression.right)
+        return MockQuery([item for item in self.items if label in item.name])
+
+    def all(self):
+        return self.items
+
+mock_meals = [Meals('Meal 1', '100'), Meals('Meal 2', '200'), Meals('Meal 3', '300'), Meals('Meal 4', '400'), Meals('Meal 5', '500')]
+mock_user_calories = UserCalories('None', '2000')
+
 def test_choose_meals_for_user(mock_meals, mock_user_calories):
     with patch("web.meal_planner.random.uniform", side_effect=[1.0, 1.0, 0.7, 1.3, 0.7, 1.3]) as mock_random_uniform:
-        Meals.query = MagicMock()
+        Meals.query = MockQuery(mock_meals)  # Initialize MockQuery with the mock_meals
         Meals.query.join.return_value = Meals.query
-        
-        def mock_filter(label_expression):
-            label = str(label_expression.right)  # Extract the label value
-            return [meal for meal in mock_meals if label in meal.name]
-        
-        Meals.query.filter.side_effect = mock_filter
-        Meals.query.all.return_value = mock_meals
 
         with patch("web.meal_planner.UserCalories.query.filter_by") as mock_filter_by:
             mock_filter_by.return_value.first.return_value = mock_user_calories
@@ -38,3 +45,4 @@ def test_choose_meals_for_user(mock_meals, mock_user_calories):
             assert breakfast_meal[1] >= 0.7 and breakfast_meal[1] <= 1.3
             assert lunch_meal[1] >= 0.7 and lunch_meal[1] <= 1.3
             assert dinner_meal[1] >= 0.7 and dinner_meal[1] <= 1.3
+
