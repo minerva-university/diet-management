0ec554db73c78414320ddcc78bff5bd900a9ae90
Shaghayegh <93452403+Shadimrad@users.noreply.github.com>
2023-04-19 22:52:22 +0530

    Update test_models.py

--- unit_test/test_models.py
+++ unit_test/test_models.py
@@ -41,5 +41,77 @@ def test_user_get_bmi(test_app):
 
     fetched_user = User.query.filter_by(email='john.doe@example.com').first()
     assert fetched_user.get_bmi == round(80 / (180 / 100) ** 2, 2)
+def test_user_weight_over_time(test_app):
+    user = User(name='John Doe', email='john.doe@example.com', password='testpassword')
+    db.session.add(user)
+    db.session.commit()
+
+    user_weight = UserWeightOverTime(user_id=user.id, weight=80)
+    db.session.add(user_weight)
+    db.session.commit()
+
+    fetched_user_weight = UserWeightOverTime.query.filter_by(user_id=user.id).first()
+    assert fetched_user_weight is not None
+    assert fetched_user_weight.weight == 80
+    assert fetched_user_weight.user == user
+
+def test_user_calories_over_time(test_app):
+    user = User(name='John Doe', email='john.doe@example.com', password='testpassword')
+    db.session.add(user)
+    db.session.commit()
+
+    user_calories = UserCaloriesOverTime(user_id=user.id, calories=2500)
+    db.session.add(user_calories)
+    db.session.commit()
+
+    fetched_user_calories = UserCaloriesOverTime.query.filter_by(user_id=user.id).first()
+    assert fetched_user_calories is not None
+    assert fetched_user_calories.calories == 2500
+    assert fetched_user_calories.user == user
+
+def test_user_calories(test_app):
+    user = User(name='John Doe', email='john.doe@example.com', password='testpassword')
+    db.session.add(user)
+    db.session.commit()
+
+    user_calories = UserCalories(user_id=user.id, calories=2500)
+    db.session.add(user_calories)
+    db.session.commit()
+
+    fetched_user_calories = UserCalories.query.filter_by(user_id=user.id).first()
+    assert fetched_user_calories is not None
+    assert fetched_user_calories.calories == 2500
+    assert fetched_user_calories.user == user
+
+def test_user_history(test_app):
+    user = User(name='John Doe', email='john.doe@example.com', password='testpassword')
+    db.session.add(user)
+    db.session.commit()
+
+    user_history = UserHistory(user_id=user.id, weight=80, height=180, age=25, activity_level='Moderately Active', gender='Male', goal='Lose Weight')
+    db.session.add(user_history)
+    db.session.commit()
+
+    fetched_user_history = UserHistory.query.filter_by(user_id=user.id).first()
+    assert fetched_user_history is not None
+    assert fetched_user_history.weight == 80
+    assert fetched_user_history.height == 180
+    assert fetched_user_history.age == 25
+    assert fetched_user_history.activity_level == 'Moderately Active'
+    assert fetched_user_history.gender == 'Male'
+    assert fetched_user_history.goal == 'Lose Weight'
+    assert fetched_user_history.user == user
+
+def test_meals(test_app):
+    meal = Meals(name='Pizza', calories=2000, serving_size=1, recipe='Pizza dough, tomato sauce, cheese, toppings')
+    db.session.add(meal)
+    db.session.commit()
+
+    fetched_meal = Meals.query.filter_by(name='Pizza').first()
+    assert fetched_meal is not None
+    assert fetched_meal.name == 'Pizza'
+    assert fetched_meal.calories == 2000
+    assert fetched_meal.serving_size == 1
+    assert fetched_meal.recipe == 'Pizza dough, tomato sauce, cheese, toppings'
 
 # To be continued
