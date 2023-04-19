ef3e8404b7994951dd00bc58f4fef558e1d6165b
Shaghayegh <93452403+Shadimrad@users.noreply.github.com>
2023-04-19 13:36:19 +0530

    Update meal_planner_test.py

--- unit_test/meal_planner_test.py
+++ unit_test/meal_planner_test.py
@@ -21,14 +21,17 @@ def test_choose_meals_for_user(mock_meals, mock_user_calories):
         Meals.query.join.return_value = Meals.query
         Meals.query.filter.return_value = Meals.query
         Meals.query.all.return_value = mock_meals
-        UserCalories.query.filter_by.return_value.first.side_effect = [mock_user_calories]
+        
+        with patch("web.meal_planner.UserCalories.query.filter_by") as mock_filter_by:
+            mock_filter_by.return_value.first.return_value = mock_user_calories
 
-        breakfast_meal, lunch_meal, dinner_meal = choose_meals_for_user(1)
+            breakfast_meal, lunch_meal, dinner_meal = choose_meals_for_user(1)
 
-        assert breakfast_meal[0] in mock_meals
-        assert lunch_meal[0] in mock_meals
-        assert dinner_meal[0] in mock_meals
-        assert breakfast_meal[1] >= 0.7 and breakfast_meal[1] <= 1.3
-        assert lunch_meal[1] >= 0.7 and lunch_meal[1] <= 1.3
-        assert dinner_meal[1] >= 0.7 and dinner_meal[1] <= 1.3
+            assert breakfast_meal[0] in mock_meals
+            assert lunch_meal[0] in mock_meals
+            assert dinner_meal[0] in mock_meals
+            assert breakfast_meal[1] >= 0.7 and breakfast_meal[1] <= 1.3
+            assert lunch_meal[1] >= 0.7 and lunch_meal[1] <= 1.3
+            assert dinner_meal[1] >= 0.7 and dinner_meal[1] <= 1.3
+
 
