b6367aea44b7c4d360f103412c8e70cd440084ec
Shaghayegh <93452403+Shadimrad@users.noreply.github.com>
2023-04-19 13:58:46 +0530

    Update meal_planner_test.py

--- unit_test/meal_planner_test.py
+++ unit_test/meal_planner_test.py
@@ -19,10 +19,12 @@ def test_choose_meals_for_user(mock_meals, mock_user_calories):
     with patch("web.meal_planner.random.uniform", side_effect=[1.0, 1.0, 0.7, 1.3, 0.7, 1.3]) as mock_random_uniform:
         ...
         breakfast_meal, lunch_meal, dinner_meal = choose_meals_for_user(1)
+        
+        mock_meals_names = [meal.name for meal in mock_meals]
 
-        assert breakfast_meal[0] in mock_meals
-        assert lunch_meal[0] in mock_meals
-        assert dinner_meal[0] in mock_meals
+        assert breakfast_meal[0].name in mock_meals_names
+        assert lunch_meal[0].name in mock_meals_names
+        assert dinner_meal[0].name in mock_meals_names
         assert breakfast_meal[1] >= 0.7 and breakfast_meal[1] <= 1.3
         assert lunch_meal[1] >= 0.7 and lunch_meal[1] <= 1.3
         assert dinner_meal[1] >= 0.7 and dinner_meal[1] <= 1.3
