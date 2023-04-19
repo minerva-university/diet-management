da97c5b304f8ba1efe2a52e37f71642c0221bba0
Shaghayegh <93452403+Shadimrad@users.noreply.github.com>
2023-04-19 13:13:13 +0530

    Update form_test.py

--- unit_test/form_test.py
+++ unit_test/form_test.py
@@ -1,47 +1,30 @@
 import pytest
-from wtforms.validators import ValidationError
-from web.forms import (
-    RegistrationForm,
-    LoginForm,
-    UpdateAccountForm,
-    CalculateCalories,
-    RequestResetForm,
-    ResetPasswordForm,
-    WeightTimeFilterForm,
-    CaloriesTimeFilterForm,
-)
+from web import app, db
 from web.models import User
-from web import app  # imports the Flask app instance
+from web.forms import LoginForm
 
-# Mock data for testing
-users = [
-    User(id=1, name="Test User", email="test@example.com", password="testpassword"),
-    User(id=2, name="Another User", email="another@example.com", password="anotherpassword"),
-]
-
-# Fixture to mock User query
-@pytest.fixture
-def mock_user_query(monkeypatch):
-    def mock_filter_by(*args, **kwargs):
+class MockQuery:
+    def filter_by(self, **kwargs):
         return mock_first
 
-    monkeypatch.setattr(User, "query", lambda: None)
-    monkeypatch.setattr(User.query, "filter_by", mock_filter_by)
+@pytest.fixture
+def mock_user_query(monkeypatch):
+    mock_query = MockQuery()
+    monkeypatch.setattr(User, "query", mock_query)
 
-def mock_first(*args, **kwargs):
-    email = kwargs["email"]
-    for user in users:
-        if user.email == email:
-            return user
-    return None
+def mock_first():
+    user = User(id=1, username="testuser", email="test@example.com")
+    user.set_password("testpassword")
+    return user
 
-def test_registration_form_validates_email(mock_user_query):
-    form = RegistrationForm(email="test@example.com")
-    with pytest.raises(ValidationError, match="That email is taken. Please choose a different one."):
-        form.validate_email(form.email)
+def test_login_form_valid_credentials(mock_user_query):
+    with app.test_request_context():  # create a request context
+        with app.app_context():  # create an application context
+            form = LoginForm(email="test@example.com", password="testpassword")
+            assert form.validate() == True
 
-def test_login_form():
-    with app.app_context():  # create an application context for the test
-        form = LoginForm(email="test@example.com", password="testpassword")
-        assert form.email.data == "test@example.com"
-        assert form.password.data == "testpassword"
+def test_login_form_invalid_credentials(mock_user_query):
+    with app.test_request_context():  # create a request context
+        with app.app_context():  # create an application context
+            form = LoginForm(email="test@example.com", password="wrongpassword")
+            assert form.validate() == False
