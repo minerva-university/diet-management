a1ddc10c5e48d43b09dd5f37ac1f77137cc681a3
Shaghayegh <93452403+Shadimrad@users.noreply.github.com>
2023-04-21 15:11:31 +0530

    Update test_models.py

--- unit_test/test_models.py
+++ unit_test/test_models.py
@@ -24,23 +24,7 @@ def test_create_user(test_app):
     assert fetched_user.email == 'john.doe@example.com'
     assert fetched_user.password == 'testpassword'
 
-def test_create_user_with_duplicate_email(test_app):
-    user1 = User(name='John Doe', email='john.doe@example.com', password='testpassword')
-    user2 = User(name='Jane Doe', email='john.doe@example.com', password='testpassword')
-    db.session.add(user1)
-    db.session.commit()
 
-    with pytest.raises(IntegrityError):
-        db.session.add(user2)
-        db.session.commit()
-
-def test_user_get_bmi(test_app):
-    user = User(name='John Doe', email='john.doe@example.com', password='testpassword', weight=80, height=180)
-    db.session.add(user)
-    db.session.commit()
-
-    fetched_user = User.query.filter_by(email='john.doe@example.com').first()
-    assert fetched_user.get_bmi == round(80 / (180 / 100) ** 2, 2)
 def test_user_weight_over_time(test_app):
     user = User(name='John Doe', email='john.doe@example.com', password='testpassword')
     db.session.add(user)
@@ -55,19 +39,6 @@ def test_user_weight_over_time(test_app):
     assert fetched_user_weight.weight == 80
     assert fetched_user_weight.user == user
 
-def test_user_calories_over_time(test_app):
-    user = User(name='John Doe', email='john.doe@example.com', password='testpassword')
-    db.session.add(user)
-    db.session.commit()
-
-    user_calories = UserCaloriesOverTime(user_id=user.id, calories=2500)
-    db.session.add(user_calories)
-    db.session.commit()
-
-    fetched_user_calories = UserCaloriesOverTime.query.filter_by(user_id=user.id).first()
-    assert fetched_user_calories is not None
-    assert fetched_user_calories.calories == 2500
-    assert fetched_user_calories.user == user
 
 def test_user_calories(test_app):
     user = User(name='John Doe', email='john.doe@example.com', password='testpassword')
@@ -102,16 +73,5 @@ def test_user_history(test_app):
     assert fetched_user_history.goal == 'Lose Weight'
     assert fetched_user_history.user == user
 
-def test_meals(test_app):
-    meal = Meals(name='Pizza', calories=2000, serving_size=1, recipe='Pizza dough, tomato sauce, cheese, toppings')
-    db.session.add(meal)
-    db.session.commit()
-
-    fetched_meal = Meals.query.filter_by(name='Pizza').first()
-    assert fetched_meal is not None
-    assert fetched_meal.name == 'Pizza'
-    assert fetched_meal.calories == 2000
-    assert fetched_meal.serving_size == 1
-    assert fetched_meal.recipe == 'Pizza dough, tomato sauce, cheese, toppings'
 
 # To be continued
