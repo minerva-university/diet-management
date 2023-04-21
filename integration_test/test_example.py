e2bb087d9bce0eac45dcc8ff9c651ce60d911626
Ahmed Ibrahim <81244897+ahmedibrhm@users.noreply.github.com>
2023-04-21 06:40:43 +0530

    Update test_example.py

--- integration_test/test_example.py
+++ integration_test/test_example.py
@@ -2,5 +2,5 @@ import requests
 
 
 def test_basic_request():
-    r = requests.get('http://host.docker.internal:5000/')
+    r = requests.get('http://172.17.0.1:5000/')
     assert r.status_code == 200
